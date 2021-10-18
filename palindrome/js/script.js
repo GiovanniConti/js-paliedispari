e9c78e82a55846e657cfe41cfc1976ed26dea734
GiovanniConti <giovanni.conti@live.it>
2021-10-18 20:01:55 +0200

    Added and completed html and css

--- palindrome/js/script.js
+++ palindrome/js/script.js
@@ -1,17 +1,31 @@
 
-const userString = prompt("Inserisci una parola: ");
-let revertString = "";
+const btn = document.getElementById("btn");
 
-// revert user string
-for(i= (userString.length - 1); i >= 0; i--){
-    revertString = revertString + userString[i];
+// added event listener to get the value of the userText at click and call the IsPalindrome function
+btn.addEventListener("click", function(){
+    const userText = document.getElementById("userText").value;
+    // console.log(userText);
+    // alert the return of IsPalindrome function
+    alert(IsPalindrome(userText));
+    
+});
 
-    console.log("Valore stringa durante il ciclo", revertString);
-}
 
-// compare the userString and the revertString and Display the result
-if(userString === revertString){
-    alert("La parola inserita è palindroma");
-} else{
-    alert("La parola inserita NON è palindroma");
+function IsPalindrome(userString){
+    let revertString = "";
+    let result;
+
+    // revert user string
+    for(i= (userString.length - 1); i >= 0; i--){
+        revertString = revertString + userString[i];    
+        // console.log("Valore stringa durante il ciclo", revertString);
+    }
+    
+    // compare the userString and the revertString and return the value of result
+    if(userString === revertString){
+        result = "La parola inserita è palindroma";
+    } else{
+        result = "La parola inserita NON è palindroma";
+    }
+    return result;
 }
