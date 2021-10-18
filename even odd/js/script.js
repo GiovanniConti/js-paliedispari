fc1e5e0d792f867744da09196f18807952385666
GiovanniConti <giovanni.conti@live.it>
2021-10-18 19:31:47 +0200

    Final solution Even&Odd

--- even odd/js/script.js
+++ even odd/js/script.js
@@ -1,16 +1,50 @@
 
-const userNumber = prompt("Inserisci un numero:");
-const cpuNumber = Math.ceil(Math.random() * 5);
+const getUserNumber = document.getElementById("userNumber");
+let userNumber;
+let cpuNumber = Math.ceil(Math.random() * 5);
+const btnEven = document.getElementById("even");
+const btnOdd = document.getElementById("odd");
 
-console.log(cpuNumber);
+btnEven.addEventListener("click", function(){
+    userNumber = getUserNumber.value;
+    // console.log("Valore di userNumber",userNumber)
 
-const sum = parseInt(userNumber) + cpuNumber;
-console.log(sum)
+    alert(EvenorOdd(userNumber, cpuNumber, true));
+});
 
-if(isNaN(sum)){
-    alert("VALORE INSERITO NON VALIDO.")
-} else if(sum %2 === 0){
-    alert("È uscito pari");
-} else{
-    alert("È uscito dispari");
+btnOdd.addEventListener("click", function(){
+    userNumber = getUserNumber.value;
+    // console.log("Valore di userNumber",userNumber)
+    
+    alert(EvenorOdd(userNumber, cpuNumber, false))
+});
+
+
+function EvenorOdd(num1, num2, even){
+
+    let sum = parseInt(num1) + num2;
+    let result;
+    
+    // console.log("Valore di num1", num1);
+    // console.log("Valore di num2", num2);
+    // console.log("Valore di nim1 + num2", sum)
+    if(num1 > 5){
+        result ="NUMERO INSERITO NON VALIDO.";
+    } else{
+        if(sum %2 === 0){
+            if(even == true){
+                result = "HAI VINTO! È uscito pari";
+            } else{
+                result = "HAI PERSO! È uscito pari";
+            }
+        } else{
+            if(even == true){
+                result = "HAI PERSO! È uscito dispari.";
+            } else{
+                result = "HAI VINTO! È uscito dispari";
+            }
+        }
+    }
+
+    return result;
 }
